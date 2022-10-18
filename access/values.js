3220b9a493095f2e64bc65c6518330207e87c3ec
KARNELkoder <106372780+KARNELkoder@users.noreply.github.com>
2022-10-18 14:36:14 +0300

    Update values.js

--- access/values.js
+++ access/values.js
@@ -3,8 +3,8 @@ var isRightsEnabled = true; // Права
 var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
-var birthdate = "05.06.2004"; // Дата рождения
-var fullname = "Гричковский Владислав Владимирович" // ФИО
+var birthdate = "05.01.2003"; // Дата рождения
+var fullname = "Гордєєв Святослав Сергійович" // ФИО
 var name = "Имя" // Имя
 
 // Паспорт
